9929cfe42c0b8de099173fe030a37709d7eb80b2
Daniel_Fabricio Nieves_Tacuri <fabricionieves2004@gmail.com>
2023-07-17 09:40:48 -0500

    first commit

---
+++ src/main/java/edu/ups/ec/estructura/arboles/ejemplo/EstructutaParcticaContactos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ups.ec.estructura.arboles.ejemplo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EstructutaParcticaContactos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
